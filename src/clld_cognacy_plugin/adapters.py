49fda7503d67eda4c52ff3f1c9c56247e0b16c7e
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2019-01-16 13:49:14 +0100

    quickfix to avoid NoneType exception while displaying cognatesets

--- src/clld_cognacy_plugin/adapters.py
+++ src/clld_cognacy_plugin/adapters.py
@@ -26,5 +26,5 @@ class GeoJsonCognateset(GeoJson):
     def feature_properties(self, ctx, req, valueset):
         values = [co.counterpart for co in ctx.cognates]
         return {
-            'label': ', '.join(v.name for v in valueset.values if v in values)
+            'label': ', '.join(v.name for v in valueset.values if v in values and v.name)
             or self.get_language(ctx, req, valueset).name}
