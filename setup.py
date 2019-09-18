a32bf9d9dab7c756e32e3345ab94eb1de2b695d8
xrotwang <xrotwang@googlemail.com>
2019-09-18 12:10:01 +0200

    0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clld-cognacy-plugin',
-    version='0.1.1',
+    version='0.2.0',
     description='clld-cognacy-plugin',
     classifiers=[
         "Programming Language :: Python",
