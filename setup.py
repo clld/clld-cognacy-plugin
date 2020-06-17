946aa12475b2a3848ac6233ec024f2dca0f67f3f
xrotwang <xrotwang@googlemail.com>
2020-06-17 08:24:14 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clld-cognacy-plugin',
-    version='0.2.1',
+    version='0.2.2.dev0',
     description='clld-cognacy-plugin',
     classifiers=[
         "Programming Language :: Python",
